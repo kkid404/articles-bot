db1f2befe62315827f40fdb547e10167b308f4a1
Andrew Gilgenberg <vizzy1488@gmail.com>
2024-07-16 23:32:15 +0300

    Сделал кнопку назад и ее логику в обтправке вопроса

--- src/scenes/sendQuestion.scene.js
+++ src/scenes/sendQuestion.scene.js
@@ -4,15 +4,27 @@ const ruMessage = require('../lang/ru.json');
 const questionService = require('../services/question.service');
 const userService = require('../services/user.service');
 const { start } = require('../keyboards/start.keyboard');
+const { back } = require('../keyboards/back.keyboard');
+
 
 
 const SendQuestionScene = new BaseScene('sendQuestion');
 
 SendQuestionScene.enter(async (ctx) => {
-    await ctx.reply(ruMessage.message.send_question);
+    await ctx.reply(ruMessage.message.send_question, back());
 });
 
 SendQuestionScene.on('text', async (ctx) => {
+    const userInput = ctx.message.text;
+
+    if (userInput == ruMessage.keyboard.back[0]) {
+        
+        await ctx.scene.enter('backScene')
+        ctx.scene.leave();
+        return
+    }
+
+
     const question = ctx.message.text;
     let user = await userService.getByIdUser(ctx.from.id);
     try{
